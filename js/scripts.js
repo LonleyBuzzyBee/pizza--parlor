6474018afe0cb805558e353e36061d318b0d0c5e
mai cook <mailiecook@yahoo.com>
2020-04-10 12:59:08 -0700

    create forEach loop to gather checkbox values, and push into an array

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,22 @@
 $(document).ready(function() {
   $("#form2").submit(function(event) {
     event.preventDefault();
-    var v1Input = parseInt($("input#v-1:checked").val());
-    var v2Input = parseInt($("input#v-2:checked").val());
-    var v3Input = parseInt($("input#v-3:checked").val());
-    var v4Input = parseInt($("input#v-4:checked").val());
-    var vegTop = v1Input + v2Input + v3Input + v4Input;
-    console.log(v1Input);
+    var getChecks = checkBox();
+    console.log(getChecks);
+function checkBox(){
+    var vegInputs = document.querySelectorAll(`input[name="vegetable"]:checked`);
+    var vegTop = []; 
     console.log(vegTop);
+    console.log(vegInputs);
+    
+    vegInputs.forEach((vegInput) => {
+      vegTop.push(vegInput.value);
+      console.log(vegInput);
+    });
+    return vegTop;  
+  }
+   
+    
+
   });
 });
